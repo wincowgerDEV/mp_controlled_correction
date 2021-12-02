c263bc9a456a8a88c24cfd409ab8f152e3eda73f
Win Cowger <wincowger@gmail.com>
2021-12-01 18:14:34 -0800

    added positive control correction to add particles when some are not recovered based on morphology and just mean recovery.

--- correction_code.R
+++ correction_code.R
@@ -1,6 +1,6 @@
 #libraries ----
 library(dplyr)
-library(skimr)
+#library(skimr)
 
 #dummy data creation ----
 field_data <- tibble(
@@ -19,6 +19,14 @@ blank_control_data <- tibble(
   size = sample(c("<10", "10-100", "100-1000"), size = 100, replace = T)
 )
 
+positive_control_data <- expand.grid(
+  #blank_particle_id = 1:100,
+  blank_sample_id = 1:3,
+  morphology = c("fiber", "film", "sphere", "fragment"), 
+  color = c("green", "red", "blue", "black"), 
+  size = c("<10", "10-100", "100-1000")
+  ) %>%
+  mutate(recovery = sample(0:100, size =  nrow(.), replace = T)/100)
 #add spike?
 
 #calculate the blank correction measures ----
@@ -60,3 +68,36 @@ for(row in 1:nrow(blank_correction_morphology)){
 
 field_data_corrected_morphology <- field_data %>%
   anti_join(remove_these)
+
+#positive control correction ----
+
+#simple mean recovery correction
+mean_recovery <- positive_control_data %>%
+  pull(recovery) %>%
+  mean()
+
+field_data_corrected_positive <- field_data %>%
+  group_by(sample_id) %>%
+  slice_sample(prop = mean_recovery) %>%
+  ungroup() %>%
+  bind_rows(field_data)
+
+#recovery by morphology correction
+recovery_correction_morphology <- positive_control_data %>%
+  group_by(morphology) %>%
+  summarise(mean = mean(recovery)) %>%
+  ungroup()
+
+add_these <- field_data[0,]
+for(row in 1:nrow(recovery_correction_morphology)){
+  add_these <- field_data %>%
+    filter(morphology == recovery_correction_morphology[[row,"morphology"]]) %>%
+    group_by(sample_id) %>%
+    slice_sample(prop = recovery_correction_morphology[[row,"mean"]]) %>%
+    ungroup() %>%
+    #select(particle_id) %>%
+    bind_rows(add_these)
+}
+
+field_data_corrected_positive_morphology <- field_data %>%
+  bind_rows(add_these)
